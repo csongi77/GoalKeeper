e512dc55a29f519744a8f620c61415cccdd8e742
csongi77 <horvath.csongor@outlook.com>
2018-02-16 18:27:23 +0100

    modification of field names in order for naming conventions

--- app/src/main/java/com/example/android/goalkeeper/MainActivity.java
+++ app/src/main/java/com/example/android/goalkeeper/MainActivity.java
@@ -2,31 +2,30 @@ package com.example.android.goalkeeper;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // declaring variables & TextView variable names
-    private static final String A_RESULT="a_result";
-    private static final String B_RESULT="b_result";
-    private static final String A_YELLOW="a_yellow";
-    private static final String B_YELLOW="b_yellow";
-    private static final String A_RED="a_red";
-    private static final String B_RED="b_red";
+    private static final String BUNDLE_A_RESULT ="BUNDLE_A_RESULT";
+    private static final String BUNDLE_B_RESULT ="BUNDLE_B_RESULT";
+    private static final String BUNDLE_A_YELLOW ="BUNDLE_A_YELLOW";
+    private static final String BUNDLE_B_YELLOW ="BUNDLE_B_YELLOW";
+    private static final String BUNDLE_A_RED ="BUNDLE_A_RED";
+    private static final String BUNDLE_B_RED ="BUNDLE_B_RED";
 
-    private int teamA_goal;
-    private int teamB_goal;
-    private int teamA_yellowCard;
-    private int teamB_yellowCard;
-    private int teamA_redCard;
-    private int teamB_redCard;
-    private TextView team_a_result,
-            team_b_result,
-            team_a_red_number,
-            team_b_red_number,
-            team_a_yellow_number,
-            team_b_yellow_number;
+    private int mTeamA_goal;
+    private int mTeamB_goal;
+    private int mTeamA_yellowCard;
+    private int mTeamB_yellowCard;
+    private int mTeamA_redCard;
+    private int mTeamB_redCard;
+    private TextView mTextViewTeamAResult,
+            mTextViewTeamBResult,
+            mTextViewTeamARedNumber,
+            mTextViewTeamBRedNumber,
+            mTextViewTeamAYellowNumber,
+            mTextViewTeamBYellowNumber;
 
 
     @Override
@@ -34,50 +33,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initializing
-        teamA_goal = 0;
-        teamB_goal = 0;
-        teamA_yellowCard = 0;
-        teamB_yellowCard = 0;
-        teamA_redCard = 0;
-        teamB_redCard = 0;
-        team_a_result = (TextView) findViewById(R.id.team_a_result);
-        team_b_result = (TextView) findViewById(R.id.team_b_result);
-        team_a_red_number = (TextView) findViewById(R.id.team_a_red_number);
-        team_b_red_number = (TextView) findViewById(R.id.team_b_red_number);
-        team_a_yellow_number = (TextView) findViewById(R.id.team_a_yellow_number);
-        team_b_yellow_number = (TextView) findViewById(R.id.team_b_yellow_number);
+        mTeamA_goal = 0;
+        mTeamB_goal = 0;
+        mTeamA_yellowCard = 0;
+        mTeamB_yellowCard = 0;
+        mTeamA_redCard = 0;
+        mTeamB_redCard = 0;
+        mTextViewTeamAResult = (TextView) findViewById(R.id.text_view_team_a_result);
+        mTextViewTeamBResult = (TextView) findViewById(R.id.text_view_team_b_result);
+        mTextViewTeamARedNumber = (TextView) findViewById(R.id.text_view_team_a_red_number);
+        mTextViewTeamBRedNumber = (TextView) findViewById(R.id.text_view_team_b_red_number);
+        mTextViewTeamAYellowNumber = (TextView) findViewById(R.id.text_view_team_a_yellow_number);
+        mTextViewTeamBYellowNumber = (TextView) findViewById(R.id.text_view_team_b_yellow_number);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         //saving current results state
-        outState.putInt(A_RESULT,teamA_goal);
-        outState.putInt(B_RESULT,teamB_goal);
-        outState.putInt(A_YELLOW,teamA_yellowCard);
-        outState.putInt(B_YELLOW,teamB_yellowCard);
-        outState.putInt(A_RED,teamA_redCard);
-        outState.putInt(B_RED,teamB_redCard);
+        outState.putInt(BUNDLE_A_RESULT, mTeamA_goal);
+        outState.putInt(BUNDLE_B_RESULT, mTeamB_goal);
+        outState.putInt(BUNDLE_A_YELLOW, mTeamA_yellowCard);
+        outState.putInt(BUNDLE_B_YELLOW, mTeamB_yellowCard);
+        outState.putInt(BUNDLE_A_RED, mTeamA_redCard);
+        outState.putInt(BUNDLE_B_RED, mTeamB_redCard);
         super.onSaveInstanceState(outState);
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
-      //  super.onRestoreInstanceState(savedInstanceState);
         // restoring state
-        teamA_goal=savedInstanceState.getInt(A_RESULT);
-        teamB_goal=savedInstanceState.getInt(B_RESULT);
-        teamA_yellowCard=savedInstanceState.getInt(A_YELLOW);
-        teamB_yellowCard=savedInstanceState.getInt(B_YELLOW);
-        teamA_redCard=savedInstanceState.getInt(A_RED);
-        teamB_redCard=savedInstanceState.getInt(B_RED);
+        mTeamA_goal =savedInstanceState.getInt(BUNDLE_A_RESULT);
+        mTeamB_goal =savedInstanceState.getInt(BUNDLE_B_RESULT);
+        mTeamA_yellowCard =savedInstanceState.getInt(BUNDLE_A_YELLOW);
+        mTeamB_yellowCard =savedInstanceState.getInt(BUNDLE_B_YELLOW);
+        mTeamA_redCard =savedInstanceState.getInt(BUNDLE_A_RED);
+        mTeamB_redCard =savedInstanceState.getInt(BUNDLE_B_RED);
 
         // displaying current gameState
-        dislplayResult(team_a_result, teamA_goal);
-        dislplayResult(team_a_red_number, teamA_redCard);
-        dislplayResult(team_a_yellow_number, teamA_yellowCard);
-        dislplayResult(team_b_result, teamB_goal);
-        dislplayResult(team_b_red_number, teamB_redCard);
-        dislplayResult(team_b_yellow_number, teamB_yellowCard);
+        dislplayResult(mTextViewTeamAResult, mTeamA_goal);
+        dislplayResult(mTextViewTeamARedNumber, mTeamA_redCard);
+        dislplayResult(mTextViewTeamAYellowNumber, mTeamA_yellowCard);
+        dislplayResult(mTextViewTeamBResult, mTeamB_goal);
+        dislplayResult(mTextViewTeamBRedNumber, mTeamB_redCard);
+        dislplayResult(mTextViewTeamBYellowNumber, mTeamB_yellowCard);
     }
 
     /**
@@ -92,27 +90,27 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void team_a_goal(View view) {
-        dislplayResult(team_a_result, ++teamA_goal);
+        dislplayResult(mTextViewTeamAResult, ++mTeamA_goal);
     }
 
     public void team_a_redcard(View view) {
-        dislplayResult(team_a_red_number, ++teamA_redCard);
+        dislplayResult(mTextViewTeamARedNumber, ++mTeamA_redCard);
     }
 
     public void team_a_yellowcard(View view) {
-        dislplayResult(team_a_yellow_number, ++teamA_yellowCard);
+        dislplayResult(mTextViewTeamAYellowNumber, ++mTeamA_yellowCard);
     }
 
     public void team_b_goal(View view) {
-        dislplayResult(team_b_result, ++teamB_goal);
+        dislplayResult(mTextViewTeamBResult, ++mTeamB_goal);
     }
 
     public void team_b_redcard(View view) {
-        dislplayResult(team_b_red_number, ++teamB_redCard);
+        dislplayResult(mTextViewTeamBRedNumber, ++mTeamB_redCard);
     }
 
     public void team_b_yellowcard(View view) {
-        dislplayResult(team_b_yellow_number, ++teamB_yellowCard);
+        dislplayResult(mTextViewTeamBYellowNumber, ++mTeamB_yellowCard);
     }
 
     /**
@@ -120,17 +118,17 @@ public class MainActivity extends AppCompatActivity {
      */
 
     public void reset(View view) {
-        teamA_goal = 0;
-        teamB_goal = 0;
-        teamA_yellowCard = 0;
-        teamB_yellowCard = 0;
-        teamA_redCard = 0;
-        teamB_redCard = 0;
-        dislplayResult(team_a_result, teamA_goal);
-        dislplayResult(team_a_red_number, teamA_redCard);
-        dislplayResult(team_a_yellow_number, teamA_yellowCard);
-        dislplayResult(team_b_result, teamB_goal);
-        dislplayResult(team_b_red_number, teamB_redCard);
-        dislplayResult(team_b_yellow_number, teamB_yellowCard);
+        mTeamA_goal = 0;
+        mTeamB_goal = 0;
+        mTeamA_yellowCard = 0;
+        mTeamB_yellowCard = 0;
+        mTeamA_redCard = 0;
+        mTeamB_redCard = 0;
+        dislplayResult(mTextViewTeamAResult, mTeamA_goal);
+        dislplayResult(mTextViewTeamARedNumber, mTeamA_redCard);
+        dislplayResult(mTextViewTeamAYellowNumber, mTeamA_yellowCard);
+        dislplayResult(mTextViewTeamBResult, mTeamB_goal);
+        dislplayResult(mTextViewTeamBRedNumber, mTeamB_redCard);
+        dislplayResult(mTextViewTeamBYellowNumber, mTeamB_yellowCard);
     }
 }
